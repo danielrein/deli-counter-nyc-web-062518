a2a0dfd5ba2855a4859226174b1071d8d45ebc62
Daniel Ran <danielr1735@gmail.com>
2018-06-19 13:50:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    katz_deli.each_with_index do |person, index|
+      string += " #{index+1}. #{person}"
   end
+  puts string
 end
