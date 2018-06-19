a57ebe90c69fde2d04b9e82723b1c626fb93079d
Daniel Ran <danielr1735@gmail.com>
2018-06-19 13:51:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,7 @@ def line(katz_deli)
     string = "The line is currently:"
     katz_deli.each_with_index do |person, index|
       string += " #{index+1}. #{person}"
+    end
   end
   puts string
 end
