26e635f093514251815a2f74cd08a5e7a15f5ebb
Daniel Ran <danielr1735@gmail.com>
2018-06-19 13:56:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving
+  if katz_deli = []
+    string = "The line is currently empty."
+  else string = "Now serving #{katz_deli.unshift}"
+  end
+  puts string
+end
