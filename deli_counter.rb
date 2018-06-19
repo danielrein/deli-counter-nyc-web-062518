6544841021552a6712313c0896fad83311820128
Daniel Ran <danielr1735@gmail.com>
2018-06-19 14:00:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli = []
+  if katz_deli == []
     puts "There is nobody waiting to be served!"
   else
     puts = "Currently serving #{katz_deli.unshift}"
