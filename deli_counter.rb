d3ace0e6acfe9a11f10d2c87a03678e701fc756e
Daniel Ran <danielr1735@gmail.com>
2018-06-19 13:57:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 katz_deli = []
 
-def line
+def line(katz_deli)
   if katz_deli == []
     string = "The line is currently empty."
   else
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli = []
     string = "The line is currently empty."
   else string = "Now serving #{katz_deli.unshift}"
